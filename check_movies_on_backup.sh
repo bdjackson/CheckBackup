5842150be618f7e0eaa2a4191e37cf00c1796151
bdjackson <bjack3@gmail.com>
2013-04-20 16:11:57 +0200

    check for/resolve differences in files

--- check_movies_on_backup.sh
+++ check_movies_on_backup.sh
@@ -8,17 +8,45 @@
 LOCAL_MOVIES_DIR="${HOME}/Music/iTunes/iTunes Music/Movies"
 BACKUP_MOVIES_DIR="/Volumes/Drobo/Media/Movies"
 
-echo "Local movie directory:  ${LOCAL_MOVIES_DIR}"
-echo "Backup movie directory: ${BACKUP_MOVIES_DIR}"
+function check_copy_file {
+  while true
+  do
+    read -p "Do you really want to replace $2 with $1? " response
+    case $response in
+      [Yy]* ) do_copy=true
+              break
+              ;;
+      [Nn]* ) do_copy=false
+              break
+              ;;
+      * )     echo "Please respond with yes or no"
+              ;;
+    esac
+  done
+
+  if $do_copy
+  then
+    cp $1 $2
+  fi
+}
 
 # ------------------------------------------------------------------------------
-# Check all the files in LOCAL_MOVIES_DIR, and see if they exist in
-# BACKUP_MOVIES_DIR
+echo "Local movie directory:  ${LOCAL_MOVIES_DIR}"
+echo "Backup movie directory: ${BACKUP_MOVIES_DIR}"
+echo ""
+
+# ------------------------------------------------------------------------------
+# - Check all the files in LOCAL_MOVIES_DIR, and see if they exist in 
+# - BACKUP_MOVIES_DIR
+# ------------------------------------------------------------------------------
+ALL_FILES=()
 MISSING_FILES=()
 for FULL_ITUNES_FILE in "${LOCAL_MOVIES_DIR}"/*
 do
   ITUNES_FILE=$(echo $FULL_ITUNES_FILE | sed "s,${LOCAL_MOVIES_DIR}/\(.*\),\1,g")
   echo "Checking for file: $ITUNES_FILE"
+
+  ALL_FILES+=("${ITUNES_FILE}")
 
   if [ ! -f "${BACKUP_MOVIES_DIR}/${ITUNES_FILE}" ]
   then
@@ -39,10 +67,12 @@ do
 done
 echo ''
 
+# ------------------------------------------------------------------------------
+# - Back up missing files if the user wants
+# ------------------------------------------------------------------------------
 if [[ ${#MISSING_FILES[@]} > 0 ]]
 then
-  # ----------------------------------------------------------------------------
-  # Should we back up the missing files
+  MISSING_FILES=$1
   echo '-----------------------------------------------------------------------'
   DO_BACKUP=false
   while true
@@ -67,12 +97,86 @@ then
     echo "Backing up files!"
     for ((i = 0; i < ${#MISSING_FILES[@]}; i++))
     do
-        echo "  Backing up ${MISSING_FILES[$i]}"
-        cp "${LOCAL_MOVIES_DIR}/${MISSING_FILES[$i]}" \
-          "${BACKUP_MOVIES_DIR}/${MISSING_FILES[$i]}"
+      echo "  Backing up ${MISSING_FILES[$i]}"
+      cp "${LOCAL_MOVIES_DIR}/${MISSING_FILES[$i]}" "${BACKUP_MOVIES_DIR}/${MISSING_FILES[$i]}"
     done
   else
     echo "Not copying -- files are still not backed up!"
+  fi
+  echo ''
+fi
+
+# ------------------------------------------------------------------------------
+# - Do diff of files on local wrt files on backup
+# ------------------------------------------------------------------------------
+if [[ ${#ALL_FILES[@]} > 0 ]]
+then
+  echo '-----------------------------------------------------------------------'
+  DO_DIFF=false
+  while true
+  do
+    read -p "Do deep check [y/n]: " yn
+    case $yn in
+      [Yy]* ) DO_DIFF=true
+              break
+              ;;
+      [Nn]* ) DO_DIFF=false
+              break
+              ;;
+      * )     echo "Please answer yes or no."
+              ;;
+    esac
+  done
+  echo ''
+
+  # Backup if the user wants to
+  if $DO_DIFF
+  then
+    echo "Deep check of files!"
+    for ((i = 0; i < ${#ALL_FILES[@]}; i++))
+    do
+      LOCAL_FILE=${LOCAL_MOVIES_DIR}/${ALL_FILES[$i]}
+      BACKUP_FILE=${BACKUP_MOVIES_DIR}/${ALL_FILES[$i]}
+
+      echo ''
+      echo '- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'
+      echo "Doing diff of ${ALL_FILES[$i]}"
+      if diff "${LOCAL_FILE}" "${BACKUP_FILE}" >/dev/null
+      then
+        # Files are the same!!!
+        echo "Local and backup copies match"
+      else
+        # Oh no! Files differ!
+        echo "WARNING: Local and backup copies differ"
+        echo "Local file size:  $(du -h "${LOCAL_FILE}"  | awk '{print $1}')"
+        echo "Backup file size: $(du -h "${BACKUP_FILE}" | awk '{print $1}')"
+
+        # What version do we keep?
+        while true
+        do
+          read -p "Which version to keep? [(l)ocal, (b)ackup, (w)ait]: " ver
+          case $ver in
+            [Ll]* ) ver="l"
+                    echo "Copying local file to backup"
+                    check_copy_file ${LOCAL_FILE} ${BACKUP_FILE}
+                    break
+                    ;;
+            [Bb]* ) ver="b"
+                    echo "Copying backup file to local"
+                    check_copy_file ${BACKUP_FILE} ${LOCAL_FILE}
+                    break
+                    ;;
+            [Ww]* ) ver="w"
+                    break
+                    ;;
+            * )     echo "Please provide valid answer."
+                    ;;
+          esac
+        done
+      fi
+    done
+  else
+    echo "Not doing diff -- Files could be different"
   fi
   echo ''
 fi
